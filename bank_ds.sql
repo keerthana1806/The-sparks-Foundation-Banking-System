5eacc9a672b170b6d7690643ed9d001c5d51491c
keerthana1806 <84397913+keerthana1806@users.noreply.github.com>
2021-11-20 18:37:01 +0530

    Create bank_ds.sql

---
+++ bank_ds.sql
@@ -0,0 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 12, 2021 at 12:53 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+---- 
+Database: `bank_db`
+--
+
+-- -----------------------------------------
+
+----
+Table structure for table `transfer`
+--
+
+CREATE TABLE `transfer` ( `sid` int(255) NOT NULL, `sender` varchar(255) NOT NULL, `receiver` varchar(255) NOT NULL, `amount` int(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
