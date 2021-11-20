ec49af1d561ed55ca69cf141a010cc1c29ac43b9
keerthana1806 <84397913+keerthana1806@users.noreply.github.com>
2021-11-20 18:45:52 +0530

    Update bank_ds.sql

--- bank_ds.sql
+++ bank_ds.sql
@@ -30,5 +30,22 @@ Table structure for table `transfer`
 
 CREATE TABLE `transfer` ( `sid` int(255) NOT NULL, `sender` varchar(255) NOT NULL, `receiver` varchar(255) NOT NULL, `amount` int(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+---- Dumping data for table `transfer`--
 
+CREATE TABLE `users` ( `id` int(255) NOT NULL, `name` varchar(255) NOT NULL, `email` varchar(255) NOT NULL, `balance` int(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+---- Dumping data for table `users`--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES(1, 'Vaibhav', 'vaibhav@gmail.com', 5400),(2, 'Vaishnavi', 'vaishnavi@gmail.com', 0);
+
+---- Indexes for dumped tables--
+
+---- Indexes for table `transfer`--ALTER TABLE `transfer` ADD PRIMARY KEY (`sid`);
+
+---- Indexes for table `users`--ALTER TABLE `users` ADD PRIMARY KEY (`id`);
+---- AUTO_INCREMENT for dumped tables--
+
+---- AUTO_INCREMENT for table `transfer`--ALTER TABLE `transfer` MODIFY `sid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+---- AUTO_INCREMENT for table `users`--ALTER TABLE `users` MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
