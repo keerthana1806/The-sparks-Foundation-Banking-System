82a8fd4990f9e67cb4cfc5eae95e2009cebf7fe5
keerthana1806 <84397913+keerthana1806@users.noreply.github.com>
2021-11-20 18:52:16 +0530

    Update bank_ds.sql

--- bank_ds.sql
+++ bank_ds.sql
@@ -17,35 +17,84 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-
----- 
-Database: `bank_db`
+--
+-- Database: `bank_db`
 --
 
--- -----------------------------------------
+-- --------------------------------------------------------
 
-----
-Table structure for table `transfer`
+--
+-- Table structure for table `transfer`
 --
 
-CREATE TABLE `transfer` ( `sid` int(255) NOT NULL, `sender` varchar(255) NOT NULL, `receiver` varchar(255) NOT NULL, `amount` int(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `transfer` (
+  `sid` int(255) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `receiver` varchar(255) NOT NULL,
+  `amount` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
----- Dumping data for table `transfer`--
+--
+-- Dumping data for table `transfer`
+--
 
-CREATE TABLE `users` ( `id` int(255) NOT NULL, `name` varchar(255) NOT NULL, `email` varchar(255) NOT NULL, `balance` int(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `transfer` (`sid`, `sender`, `receiver`, `amount`) VALUES
+(1, 'Vaishnavi', 'Vaibhav', 400);
 
----- Dumping data for table `users`--
+-- --------------------------------------------------------
 
-INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES(1, 'Vaibhav', 'vaibhav@gmail.com', 5400),(2, 'Vaishnavi', 'vaishnavi@gmail.com', 0);
+--
+-- Table structure for table `users`
+--
 
----- Indexes for dumped tables--
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `balance` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
----- Indexes for table `transfer`--ALTER TABLE `transfer` ADD PRIMARY KEY (`sid`);
+--
+-- Dumping data for table `users`
+--
 
----- Indexes for table `users`--ALTER TABLE `users` ADD PRIMARY KEY (`id`);
----- AUTO_INCREMENT for dumped tables--
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Vaibhav', 'vaibhav@gmail.com', 5400),
+(2, 'Vaishnavi', 'vaishnavi@gmail.com', 0);
 
----- AUTO_INCREMENT for table `transfer`--ALTER TABLE `transfer` MODIFY `sid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
----- AUTO_INCREMENT for table `users`--ALTER TABLE `users` MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+--
+-- Indexes for dumped tables
+--
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Indexes for table `transfer`
+--
+ALTER TABLE `transfer`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transfer`
+--
+ALTER TABLE `transfer`
+  MODIFY `sid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
